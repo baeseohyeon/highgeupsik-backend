a933dd752e8ead64c4cbcf43e1f4f46fc76be46a
baeseohyeon <formnarago@naver.com>
2022-11-02 21:48:27 +0900

    refactor: 매퍼 필드이름 짧게 수정

--- highgeupsik-backend-api/src/main/java/com/highgeupsik/backend/api/jwt/JwtExceptionFilter.java
+++ highgeupsik-backend-api/src/main/java/com/highgeupsik/backend/api/jwt/JwtExceptionFilter.java
@@ -1,7 +1,7 @@
 package com.highgeupsik.backend.api.jwt;
 
 import static com.highgeupsik.backend.api.controller.ApiUtils.error;
-import static javax.servlet.http.HttpServletResponse.*;
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.highgeupsik.backend.api.controller.ApiError;
@@ -18,7 +18,7 @@ import org.springframework.web.filter.GenericFilterBean;
 @RequiredArgsConstructor
 public class JwtExceptionFilter extends GenericFilterBean {
 
-    private final ObjectMapper objectMapper;
+    private final ObjectMapper mapper;
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -33,7 +33,6 @@ public class JwtExceptionFilter extends GenericFilterBean {
     private void sendErrorMessage(HttpServletResponse response, String message) throws IOException {
         response.setStatus(SC_BAD_REQUEST);
         response.setContentType("application/json;charset=utf-8");
-        response.getWriter().write(this.objectMapper
-            .writeValueAsString(error(new ApiError("BAD", message))));
+        response.getWriter().write(mapper.writeValueAsString(error(new ApiError("BAD", message))));
     }
 }
