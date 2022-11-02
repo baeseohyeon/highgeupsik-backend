9ce8b83411912f2579f32696dba850bccce0b325
baeseohyeon <formnarago@naver.com>
2022-11-02 21:45:40 +0900

    feat: 어드민 서비스 회원가입 시 이벤트 호출하도록 로직 수정

--- highgeupsik-backend-api/src/main/java/com/highgeupsik/backend/api/service/user/UserAdminService.java
+++ highgeupsik-backend-api/src/main/java/com/highgeupsik/backend/api/service/user/UserAdminService.java
@@ -1,12 +1,14 @@
 package com.highgeupsik.backend.api.service.user;
 
+import static com.highgeupsik.backend.core.exception.ErrorMessages.*;
+
 import com.highgeupsik.backend.api.repository.user.UserConfirmRepository;
 import com.highgeupsik.backend.api.repository.user.UserRepository;
-import com.highgeupsik.backend.api.service.MailService;
-import com.highgeupsik.backend.core.exception.ErrorMessages;
+import com.highgeupsik.backend.core.events.JoinEvent;
 import com.highgeupsik.backend.core.exception.ResourceNotFoundException;
 import com.highgeupsik.backend.core.model.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,20 +19,20 @@ public class UserAdminService {
 
     private final UserRepository userRepository;
     private final UserConfirmRepository userConfirmRepository;
-    private final MailService mailService;
+    private final ApplicationEventPublisher publisher;
 
     public void acceptUser(Long userId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(
-            ErrorMessages.USER_NOT_FOUND));
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new ResourceNotFoundException(USER_NOT_FOUND));
         user.updateRoleUser();
         userConfirmRepository.deleteByUserId(userId);
-        mailService.sendEmail(user.getUsername(), user.getEmail(), true);
+        publisher.publishEvent(JoinEvent.ofSuccess(user.getEmail(), user.getUsername()));
     }
 
     public void rejectUser(Long userId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(
-            ErrorMessages.USER_NOT_FOUND));
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new ResourceNotFoundException(USER_NOT_FOUND));
         userConfirmRepository.deleteByUserId(userId);
-        mailService.sendEmail(user.getUsername(), user.getEmail(), false);
+        publisher.publishEvent(JoinEvent.ofFail(user.getEmail(), user.getUsername()));
     }
 }
