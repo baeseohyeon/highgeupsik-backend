7bdf37d3991ff76c667a68e584a71e91174777be
baeseohyeon <formnarago@naver.com>
2022-11-24 15:40:02 +0900

    feat: 룸 목록조회 최근메세지 순으로 수정

--- highgeupsik-backend-api/src/main/java/com/highgeupsik/backend/api/service/message/RoomQueryService.java
+++ highgeupsik-backend-api/src/main/java/com/highgeupsik/backend/api/service/message/RoomQueryService.java
@@ -1,11 +1,10 @@
 package com.highgeupsik.backend.api.service.message;
 
-import static com.highgeupsik.backend.api.utils.PagingUtils.*;
-
 import com.highgeupsik.backend.api.repository.message.RoomRepository;
 import com.highgeupsik.backend.core.dto.message.RoomDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,8 +16,8 @@ public class RoomQueryService {
     private final RoomRepository roomRepository;
     private static final int ROOM_COUNT = 10;
 
-    public Page<RoomDTO> findAllByMyId(Long userId, Integer pageNum) {
-        return roomRepository.findAllBySenderId(userId, orderByModifiedDate(pageNum, ROOM_COUNT))
+    public Page<RoomDTO> findAllBySenderId(Long userId, Integer pageNum) {
+        return roomRepository.findAllBySenderId(userId, PageRequest.of(pageNum, ROOM_COUNT))
             .map(RoomDTO::new);
     }
 }
